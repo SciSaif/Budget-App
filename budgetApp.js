12d8aa99b3fe6b75a052fcecbc53fe0f63ed09be
Saifullah <saiflll284@gmail.com>
2019-07-15 20:55:23 +0530

    uyu

--- budgetApp.js
+++ budgetApp.js
@@ -1,5 +1,7 @@
 const trash = document.querySelector('.fa-trash');
 const li = document.querySelector('li');
+const addBox = document.querySelector('.add-box');
+const addBoxOpt = document.querySelector('.add-box-opt');
 
 trash.addEventListener('click', () =>{
     li.classList.add('animateli');
@@ -9,3 +11,15 @@ trash.addEventListener('click', () =>{
     
     },500)
 })
+
+function openDisplay() {
+    if (addBoxOpt.classList.contains('animate-add-box-open')) {
+        addBoxOpt.classList.replace('animate-add-box-open','animate-add-box-close')
+    }else
+    {
+        addBoxOpt.classList.add('animate-add-box-open')
+        addBoxOpt.classList.replace('animate-add-box-close','animate-add-box-open')
+    }
+}
+
+addBox.addEventListener('click', openDisplay)
