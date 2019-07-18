c9f81853e8bf919f9bb45033f3c425f50043ad4f
Saifullah <saiflll284@gmail.com>
2019-07-19 03:32:22 +0530

    fgfg

--- budgetApp.js
+++ budgetApp.js
@@ -4,6 +4,12 @@ const addBox = document.querySelector('.add-box');
 const addBoxOpt = document.querySelector('.add-box-opt');
 const saif = document.querySelector('.saif');
 const bodyb = document.querySelector('body');
+const changeBudgetInput = document.querySelector('.budget-change-val');
+const changeBudgetBtn = document.querySelector('.change-budget-btn');
+const changeBudgetLabel = document.querySelector('.change-budget-label');
+const changeBudgetBtnArrow = document.querySelector('.fa-arrow-right');
+
+
 
 const colours = ["#1ee9df", "#087e78", "#074ac7", "#074ac7", "#0f45a8", "#a80f9b", "#a80f4f", "#d31c1c", "#04c96d", "#fafafa", "#05e736", "#b6e705"];
 let random = Math.floor(Math.random()*colours.length);
@@ -30,7 +36,7 @@ trash.addEventListener('click', hideitem)
 function openDisplay() {
     if (addBoxOpt.classList.contains('animate-add-box-open')) {
         addBoxOpt.classList.replace('animate-add-box-open','animate-add-box-close')
-        addBoxOpt.classList.add('invisible')
+        addBoxOpt.classList.add('invisible');
     }else
     {
 
@@ -41,3 +47,13 @@ function openDisplay() {
 }
 
 addBox.addEventListener('click', openDisplay)
+
+function changeBudget() {
+    changeBudgetInput.classList.toggle('width-80vw');
+    changeBudgetLabel.classList.toggle('width-0');
+    changeBudgetBtn.classList.toggle('width-20vw');
+    changeBudgetBtnArrow.classList.toggle('rotate-90');
+}
+
+changeBudgetBtn.addEventListener('click', changeBudget);
+
