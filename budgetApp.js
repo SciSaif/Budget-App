c80ed57087f8d5b365027498f9fa9e290d9323cc
Saifullah <saiflll284@gmail.com>
2019-07-18 12:47:13 +0530

    ghghgh

--- budgetApp.js
+++ budgetApp.js
@@ -2,6 +2,19 @@ const trash = document.querySelector('.fa-trash');
 const li = document.querySelector('li');
 const addBox = document.querySelector('.add-box');
 const addBoxOpt = document.querySelector('.add-box-opt');
+const saif = document.querySelector('.saif');
+const bodyb = document.querySelector('body');
+
+const colours = ["#1ee9df", "#087e78", "#074ac7", "#074ac7", "#0f45a8", "#a80f9b", "#a80f4f", "#d31c1c", "#04c96d", "#fafafa", "#05e736", "#b6e705"];
+let random = Math.floor(Math.random()*colours.length);
+const color = colours[random];
+var colorLine = `linear-gradient(to right,rgb(36, 35, 35), ${color})`;
+bodyb.style.background = colorLine;
+
+
+setTimeout(()=>{
+    saif.classList.add('invisible')
+},3000)
 
 function hideitem() {
 
